2045186f5b7d7153bd07e76282ab2e15363a4820
czer <philip@czer.eu>
2024-01-12 02:28:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,11 +6,13 @@ start_dir="/Users/user/uni/sorted"
 
 find "$start_dir" -type d | while read -r dir
 do
-
   current_dir=$(basename "$dir")
   parent_dir=$(basename "$(dirname "$dir")")
+  current_date=$(date +%Y%m%d) 
   if ls "$dir"/*.pdf 1> /dev/null 2>&1; then
-      rm "$dir"/"$parent_dir"-"$current_dir".pdf
-      pdfunite "$dir"/*.pdf "$dir"/"$parent_dir"-"$current_dir".pdf
+      if [ -f "$dir"/"$parent_dir"-"$current_dir"-"$current_date".pdf ]; then
+        rm "$dir"/"$parent_dir"-"$current_dir"*.pdf
+      fi
+      pdfunite "$dir"/*.pdf "$dir"/"$parent_dir"-"$current_dir"-"$current_date".pdf
   fi
 done
