b4e8fd2da4747344853336d540c3f0c38efbb24a
czer <philip@czer.eu>
2024-01-21 22:46:35 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+if ! which pdfunite > /dev/null; then
+    echo "pdfunite from poppler-utils is not installed. Please install it and try again."
+    exit 1
+fi
 ./tum-get
 
-start_dir="/Users/user/uni/sorted"
+start_dir="$1"
 
-find "$start_dir" -type d | while read -r dir
+mkdir -p "$start_dir"
+
+if [ ! -d "$start_dir" ]; then
+    echo "Error: $start_dir is not a valid directory."
+    exit 1
+fi
+
+find "$start_dir"/*/* -type d | while read -r dir
 do
   current_dir=$(basename "$dir")
   parent_dir=$(basename "$(dirname "$dir")")
-  current_date=$(date +%Y%m%d) 
+  current_date=$(date +%y%m%d)
+
+  find "$dir" -type f | while read -r file
+  do
+    new_file=$(echo "$file" | sed 's/ä/ae/g' | sed 's/ö/oe/g' | sed 's/ü/ue/g' | sed 's/Ä/Ae/g' | sed 's/Ö/Oe/g' | sed 's/Ü/Ue/g' | sed 's/ß/ss/g')
+    if [ "$file" != "$new_file" ]; then
+        mv "$file" "$new_file"
+    fi
+  done
+
   if ls "$dir"/*.pdf 1> /dev/null 2>&1; then
-      if [ -f "$dir"/"$parent_dir"-"$current_dir"-"$current_date".pdf ]; then
-        rm "$dir"/"$parent_dir"-"$current_dir"*.pdf
-      fi
-      pdfunite "$dir"/*.pdf "$dir"/"$parent_dir"-"$current_dir"-"$current_date".pdf
+    merge_file=("$start_dir"/"$parent_dir"-"$current_dir"-*.pdf)
+    if [ -e "${merge_file[0]}" ]; then
+      rm "$start_dir"/"$parent_dir"-"$current_dir"*.pdf
+    fi
+    pdfunite "$dir"/*.pdf "$start_dir"/"$parent_dir"-"$current_dir"-"$current_date".pdf
   fi
 done
